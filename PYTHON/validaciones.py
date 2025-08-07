08698373e90dd3ca0fb1ff73e4b90ff4e52ae4aa
Ivan <ivan.garcia.carbajal.17@gmail.com>
2025-08-07 16:44:01 -0600

    -a

--- PYTHON/validaciones.py
+++
@@ -1,6 +0,0 @@
-while True:
-    try:
-        select = int(input(
-            '  Elige una opcion \n Agregar Datos presiona 1 \n Visualizar datos presiona 2 \n Volver al menu principal presiona 3 :  '))
-    except ValueError:
-        print("Por favor, ingresa un número válido.")
